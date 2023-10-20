842d9feb16b60fcc129be8974fbaa383d889016d
Iqbal Shaikh <ishaikh@users.noreply.github.com>
2023-10-20 14:01:58 +0100

    ACPENG 1175 (#1)
    * ACPENG-1196 - creating amp module
    
    * ACPENG-1196 - creating amp module
    
    * ACPENG-1196 - creating amp module
    
    * ACPENG-1196 - creating amp module
    
    * ACPENG-1196 - creating amp module
    
    ---------
    
    Co-authored-by: Iqbal Shaikh <iqbal.shaikh@digital.homeoffice.gov.uk>

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "create_workspace" {
+  description = "Determines whether a workspace will be created or to use an existing workspace"
+  type        = bool
+  default     = true
+}
+
+variable "workspace_id" {
+  description = "The ID of an existing workspace to use when `create_workspace` is `false`"
+  type        = string
+  default     = ""
+}
+
+variable "workspace_name" {
+  description = "Name (Alias) of the Prometheus workspace, should be unique (for readability not enforced) in the account"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "A map of tags to add to the resources"
+  type = map(string)
+  default     = {}
+}
+
+variable "logging_configuration" {
+  description = "The logging configuration of the workspace"
+  type        = map(string)
+  default     = {}
+}
+
+variable "alert_manager_definition" {
+  description = "The alert manager definition that you want to be applied."
+  type        = string
+  default     = <<-EOT
+    alertmanager_config: |
+      route:
+        receiver: 'default'
+      receivers:
+        - name: 'default'
+  EOT
+}
+
+variable "rule_group_namespaces" {
+  description = "A map of one or more rule group namespace definitions"
+  type        = map(any)
+  default     = {}
+}
